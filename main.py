7e13ad5a458ed5254056e4e6fb23958e41e8f2a5
Ernur <92059459+decibel1030@users.noreply.github.com>
2023-03-30 18:58:47 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import speech_recognition
+
+sr = speech_recognition.Recognizer()
+# cmd start in the end voice action
+sr.pause_threshold = 0.6
+with speech_recognition.Microphone() as mic:
+    # filter noises
+    sr.adjust_for_ambient_noise(source=mic, duration=0.5)
+    audio = sr.listen(source=mic)
+    query = sr.recognize_google(audio_data=audio, language="ru-RU").lower()
+
+print(query)
+
+
+def welcome():
+    print("Hello Creator !")
+
+
+if "amadeus" in query:
+    if "amadeus начни программу" in query:
+        welcome()
+    else:
+        print("suck dick")
+
